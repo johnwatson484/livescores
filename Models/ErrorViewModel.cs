b0cfac3b4c0d77ad36534d603bcb7ec25f3a8632
John Watson <johnwatson484@hotmail.com>
2019-09-14 19:31:13 +0100

    Project added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Livescores.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
